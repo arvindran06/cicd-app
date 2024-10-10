797c574930b90177f0ac4341e29083c1071602ec
Arvindran06 <arvindran06@gmail.com>
2024-10-10 11:33:34 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+
+export default function App() {
+  const [email, setEmail] = useState('')
+  const [isFormValid, SetIsFormValid] = useState(false)
+
+
+  const checkFormValidity = () => {
+    const form = document.querySelector('form')
+    SetIsFormValid(form.checkValidity())
+  }
+
+  useEffect(() => {
+    checkFormValidity()
+  }, [])
+
+  return (
+    <Container className='my-5'>
+      <h1>Form Submission</h1>
+      <Form className='my-4' onChange={checkFormValidity}>
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            data-testid="emailInput"
+          />
+        </Form.Group>
+        <Button
+          type='submit'
+          className='btn btn-primary'
+          disabled={!isFormValid}
+          data-testid="submitButton"
+        >
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
+}
+
+
+
